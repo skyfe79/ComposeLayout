93eb476ec855f00e3edf6053760f3d8bbdaf2a5a
skyfe79 <skyfe79@gmail.com>
2024-02-01 18:45:58 +0900

    implement grid and inset items grid

---
+++ Examples/CocoaExample/CocoaExample/Extensions/NSColor+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  NSColor+Extension.swift
+//  CocoaExample
+//
+//  Created by codingmax on 2024/02/01.
+//
+
+import Foundation
+import Cocoa
+
+extension NSColor {
+    static var cornflowerBlue: NSColor {
+        return NSColor(displayP3Red: 85.0 / 255.0, green: 151.0 / 255.0, blue: 244.0 / 255.0, alpha: 1.0)
+    }
+}
