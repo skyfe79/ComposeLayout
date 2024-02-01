f1ff01a9881a713fd805739f28962b27eaee7a8b
skyfe79 <skyfe79@gmail.com>
2024-02-01 18:17:20 +0900

    implement List example

---
+++ Examples/CocoaExample/CocoaExample/Items/ListItem.swift
@@ -0,0 +1,38 @@
+//
+//  ListItem.swift
+//  CocoaExample
+//
+//  Created by codingmax on 2024/02/01.
+//
+
+import Cocoa
+
+class ListItem: NSCollectionViewItem {
+    
+    static let reuseIdentifier = NSUserInterfaceItemIdentifier("list-item-reuse-identifier")
+    
+    override var highlightState: NSCollectionViewItem.HighlightState {
+        didSet {
+            updateSelectionHighlighting()
+        }
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            updateSelectionHighlighting()
+        }
+    }
+    
+    private func updateSelectionHighlighting() {
+        if !isViewLoaded {
+            return
+        }
+        
+        let showAsHighlighted = (highlightState == .forSelection) ||
+            (isSelected && highlightState != .forDeselection) ||
+            (highlightState == .asDropTarget)
+            
+        textField?.textColor = showAsHighlighted ? .selectedTextColor : .labelColor
+        view.layer?.backgroundColor = showAsHighlighted ? NSColor.selectedControlColor.cgColor : nil
+    }
+}
